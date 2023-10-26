8c836d9564501441f30583632d1cfaed64939989
gtxzsxxk <2524395907@qq.com>
2023-10-26 23:39:23 +0800

    feat: be able to read elf header

--- src/elf_helper.cpp
+++ src/elf_helper.cpp
@@ -1,9 +1,16 @@
 //
 // Created by hanyuan on 2023/10/25.
 //
-
+#include <iostream>
 #include "elf_helper.h"
 
 void elf_helper::read_elf() {
-
+    auto *fp = fopen(elf_file_path.c_str(), "r");
+    if (fp == nullptr) {
+        std::cerr << "Cannot open file: " << elf_file_path << std::endl;
+        return;
+    }
+    Elf64_Ehdr hdr;
+    fread(&hdr,sizeof(hdr),1,fp);
+    fclose(fp);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <unistd.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "elf_helper.h"
+
+int main(int argc, char **argv) {
+    int opt;
+    const char *optstring = "e";
+    std::string elf_path;
+    while ((opt = getopt(argc, argv, optstring)) != -1) {
+        elf_path = std::move(std::string(argv[optind]));
+    }
+    if (std::empty(elf_path)) {
+        std::cerr << "No ELF file given." << std::endl;
+        return -1;
+    }
+    auto elf_handler = elf_helper(elf_path);
     return 0;
 }
