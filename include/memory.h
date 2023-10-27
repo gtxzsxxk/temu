11bdf97634973006dc4372c66340b4027186a0f7
gtxzsxxk <2524395907@qq.com>
2023-10-27 08:48:24 +0800

    fix: wrong size calculation

--- include/memory.h
+++ include/memory.h
@@ -10,16 +10,29 @@
 
 class memory {
 public:
-    uint64_t addr;
+    uint64_t base_addr;
     uint64_t *mem;
+    uint64_t length_bytes;
 
     /* The size is in MiB */
-    memory(uint64_t addr, uint64_t size) : addr(addr) {
-        mem = new uint64_t[size * 65536];
+    memory(uint64_t base_addr, uint64_t size) : base_addr(base_addr) {
+        length_bytes = size * 1024 * 1024;
+        mem = new uint64_t[length_bytes >> 3];
     }
 
     ~memory() {
         delete[] mem;
+    }
+
+    uint64_t *remap(uint64_t *from) const {
+        uint64_t cmp = (uint64_t) from;
+        if (cmp >= base_addr) {
+            uint64_t diff = cmp - base_addr;
+            if (diff <= length_bytes) {
+                return mem + (diff >> 3);
+            }
+        }
+        return nullptr;
     }
 };
 
