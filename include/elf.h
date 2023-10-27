87a55c604f25b3678ab04dc912b77a7f9acbe034
gtxzsxxk <2524395907@qq.com>
2023-10-27 08:48:41 +0800

    refactor: the cpu memory remap

--- include/elf.h
+++ include/elf.h
@@ -321,7 +321,7 @@ typedef struct elf64_shdr {
     Elf64_Word sh_name;		/* Section name, index in string tbl */
     Elf64_Word sh_type;		/* Type of section */
     Elf64_Xword sh_flags;		/* Miscellaneous section attributes */
-    Elf64_Addr sh_addr;		/* Section virtual addr at execution */
+    Elf64_Addr sh_addr;		/* Section virtual base_addr at execution */
     Elf64_Off sh_offset;		/* Section file offset */
     Elf64_Xword sh_size;		/* Size of section in bytes */
     Elf64_Word sh_link;		/* Index of another section */
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -4,9 +4,18 @@
 #include "cpu.h"
 
 void cpu::load_elf(elf_helper &handler) {
-    for()
+//    for()
 }
 
 uint64_t *cpu::memory_map(uint64_t *mem) {
+    uint64_t *tmp;
+    tmp = RAM.remap(mem);
+    if (tmp != nullptr) {
+        return tmp;
+    }
+    tmp = ROM.remap(mem);
+    if (tmp != nullptr) {
+        return tmp;
+    }
     return nullptr;
 }
