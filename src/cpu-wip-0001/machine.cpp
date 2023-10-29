108f7da8f37eb3a7a73d897eb9814342d07368fa
hanyuan <2524395907@qq.com>
2023-10-29 11:15:35 +0800

    feat: be able to detect the inst error

--- src/cpu-wip-0001/machine.cpp
+++ src/cpu-wip-0001/machine.cpp
@@ -2,6 +2,7 @@
 // Created by hanyuan on 2023/10/25.
 //
 
+#include <iostream>
 #include "machine.h"
 #include "decode.h"
 
@@ -10,7 +11,17 @@ void machine::boot() {
     while (1) {
         uint32_t inst = (*memory_map((uint64_t *) registers->program_counter)) & 0xffffffff;
         registers->debug_print();
-        inst_exec(inst, this);
+        if (inst_exec(inst, this)) {
+            break;
+        }
         registers->program_counter += 4;
+        if (registers->program_counter >= ROM.base_addr + text_size) {
+            std::cerr << "Hard Fault: code end." << std::endl;
+            break;
+        }
+        if (registers->program_counter < ROM.base_addr) {
+            std::cerr << "Hard Fault: code cannot locate." << std::endl;
+            break;
+        }
     }
 }
