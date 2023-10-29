64c6f077c832ac0a7b65360b3db1b6c6fa2ee480
gtxzsxxk <2524395907@qq.com>
2023-10-30 00:26:06 +0800

    feat: add the libc test bins

--- test/bare-metal/test_4.c
+++ test/bare-metal/test_4.c
@@ -1,17 +1,7 @@
-int arr[6];
+#include <string.h>
+
+const char *data = "Test of Qemu";
 
 int main() {
-    int max = 999;
-    arr[0] = 1;
-    arr[1] = 9;
-    arr[2] = 2;
-    arr[3] = 6;
-    arr[4] = 1;
-    arr[5] = 8;
-    for (int i = 0; i < 6; i++) {
-        if (arr[i] < max) {
-            max = arr[i];
-        }
-    }
-    return max;
+    return strlen(data);
 }
