8a68daf19d5d9f948a3f05d8fa44cbf0f0b72389
gtxzsxxk <2524395907@qq.com>
2024-02-09 00:32:33 +0800

    decode: add I S B U J type decode

--- src/decode.c
+++ src/decode.c
@@ -48,6 +48,43 @@ static void decode_type_r(uint32_t inst, uint8_t *rd, uint8_t *funct3, uint8_t *
     *funct7 = INST_EXT(31, 25);
 }
 
+static void decode_type_i(uint32_t inst, uint8_t *rd, uint8_t *funct3, uint8_t *rs1, uint16_t *imm) {
+    *rd = INST_EXT(11, 7);
+    *funct3 = INST_EXT(14, 12);
+    *rs1 = INST_EXT(19, 15);
+    *imm = INST_EXT(31, 20);
+}
+
+static void decode_type_s(uint32_t inst, uint8_t *imm, uint8_t *funct3, uint8_t *rs1, uint16_t *rs2) {
+    *imm = INST_EXT(11, 7) | (INST_EXT(31, 25) << 5);
+    *funct3 = INST_EXT(14, 12);
+    *rs1 = INST_EXT(19, 15);
+    *rs2 = INST_EXT(24, 20);
+}
+
+static void decode_type_b(uint32_t inst, uint8_t *imm, uint8_t *funct3, uint8_t *rs1, uint16_t *rs2) {
+    *imm = (INST_EXT(11, 8) << 1) |
+           (INST_EXT(30, 25) << 5) |
+           (((inst >> 7) & 0x01) << 11) |
+           (((inst >> 31) & 0x01) << 12);
+    *funct3 = INST_EXT(14, 12);
+    *rs1 = INST_EXT(19, 15);
+    *rs2 = INST_EXT(24, 20);
+}
+
+static void decode_type_u(uint32_t inst, uint8_t *imm, uint8_t *rd) {
+    *imm = (INST_EXT(31, 12) << 12);
+    *rd = INST_EXT(11, 7);
+}
+
+static void decode_type_j(uint32_t inst, uint8_t *imm, uint8_t *rd) {
+    *imm = (INST_EXT(19, 12) << 12) |
+           (INST_EXT(30, 21) << 1) |
+           (((inst >> 20) & 0x01) << 11) |
+           (((inst >> 31) & 0x01) << 20);
+    *rd = INST_EXT(11, 7);
+}
+
 static void LUI(uint32_t inst) {
 
 }
