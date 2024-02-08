8ef9cff519dfd4556b372018cd1f4988d651c011
gtxzsxxk <2524395907@qq.com>
2024-02-08 23:00:01 +0800

    mem: add memory definitions for rv32 machine

---
+++ include/parameters.h
@@ -0,0 +1,13 @@
+//
+// Created by hanyuan on 2024/2/8.
+//
+
+#ifndef TEMU_PARAMETERS_H
+#define TEMU_PARAMETERS_H
+
+#define ROM_START_ADDR 0x00000000
+#define ROM_SIZE       0x01000000
+#define RAM_START_ADDR 0x02000000
+#define RAM_SIZE       0x01000000
+
+#endif //TEMU_PARAMETERS_H
