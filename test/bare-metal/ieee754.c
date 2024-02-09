beec92d3bf50aa05072098cba344c2acc318dcd3
gtxzsxxk <2524395907@qq.com>
2024-02-10 00:45:59 +0800

    test: add integer and ieee754 tests

---
+++ test/bare-metal/ieee754.c
@@ -0,0 +1,7 @@
+int main() {
+    float a = 19.6;
+    float d = 7.8;
+    int c = 114;
+    double e = 3.14;
+    return (int) (a * d + c / e);
+}
---
+++ test/bare-metal/test2.c
@@ -0,0 +1,6 @@
+int main() {
+    int a = 0x114;
+    int b = 0x514;
+    int c = a << 12 | b;
+    return c;
+}
