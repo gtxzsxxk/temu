2c466f8d1068a62c10ef815f3ba6859acac779ee
gtxzsxxk <2524395907@qq.com>
2024-02-10 19:18:19 +0800

    test: add ieee754 float and double convert and calc test

--- test/bare-metal/ieee754.c
+++ test/bare-metal/ieee754.c
@@ -1,7 +1,13 @@
-int main() {
+int test() {
     float a = 19.6;
     float d = 7.8;
     int c = 114;
     double e = 3.14;
     return (int) (a * d + c / e);
+}
+
+int main() {
+    test();
+    __asm__("ecall");
+    return 0;
 }
