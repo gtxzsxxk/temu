2276387b4b81ba21954ea1469ea7fac6b37c836a
gtxzsxxk <2524395907@qq.com>
2024-02-11 16:16:05 +0800

    zicsr: add csrrw csrrs csrrc functions

--- src/zicsr.c
+++ src/zicsr.c
@@ -2,12 +2,13 @@
 // Created by hanyuan on 2024/2/10.
 //
 #include <stdint.h>
+#include "mem.h"
 #include "zicsr.h"
 
 uint32_t control_status_registers[CSR_SIZE] = {
-        CSR_RESET_VALUE(misa,0x40140101)
-        CSR_RESET_VALUE(mvendorid,0x00000000)
-        CSR_RESET_VALUE(mhartid,0x00000000)
+        CSR_RESET_VALUE(misa, 0x40140101)
+        CSR_RESET_VALUE(mvendorid, 0x00000000)
+        CSR_RESET_VALUE(mhartid, 0x00000000)
 
 };
 const uint32_t csr_match[CSR_SIZE] = {
@@ -48,6 +49,18 @@ const uint32_t csr_match[CSR_SIZE] = {
         CSR_MATCH_DECLARE(mconfigptr)
 };
 const uint8_t csr_index_remap[0x10] = {
+        [0x0] = 100,
+        [0x2] = 100,
+        [0x4] = 100,
+        [0x6] = 100,
+        [0x7] = 100,
+        [0x8] = 100,
+        [0x9] = 100,
+        [0xA] = 100,
+        [0xB] = 100,
+        [0xD] = 100,
+        [0xE] = 100,
+
         [0x1] = CSR_idx_sstatus,
         [0x3] = CSR_idx_mstatus,
         [0x5] = CSR_idx_scontext,
@@ -56,5 +69,69 @@ const uint8_t csr_index_remap[0x10] = {
 };
 
 uint8_t csr_get_index_by_number(uint16_t csr_number) {
+    uint8_t start_index = csr_index_remap[csr_number >> 8];
+    if (start_index == 100) {
+        /* Raise exception */
+        return -1;
+    }
+    for (; start_index < CSR_SIZE; start_index++) {
+        if (((csr_match[start_index] >> 8) & 0xFFF) == csr_number) {
+            return start_index;
+        }
+    }
+    /* Raise exception */
+    return -1;
+}
 
+uint32_t csr_read(uint16_t csr_index) {
+    /* TODO: implement read actions */
+    return control_status_registers[csr_index];
+}
+
+void csr_write(uint16_t csr_index, uint32_t value) {
+    /* TODO: implement write actions */
+    if (csr_match[csr_index] & CSR_MASK_WRITE) {
+        control_status_registers[csr_index] = value;
+    }
+}
+
+void csr_csrrw(uint8_t rs1, uint8_t rd, uint16_t csr_number) {
+    uint8_t index = csr_get_index_by_number(csr_number);
+    if (index == (uint8_t) -1) {
+        /* Raise exception */
+        return;
+    }
+    if (rd) {
+        uint32_t prev_value = csr_read(index);
+        csr_write(index, mem_register_read(rs1));
+        mem_register_write(rd, prev_value);
+    } else {
+        csr_write(index, mem_register_read(rs1));
+    }
+}
+
+void csr_csrrs(uint8_t rs1, uint8_t rd, uint16_t csr_number) {
+    uint8_t index = csr_get_index_by_number(csr_number);
+    if (index == (uint8_t) -1) {
+        /* Raise exception */
+        return;
+    }
+    uint32_t prev_value = csr_read(index);
+    mem_register_write(rd, prev_value);
+    if (rs1) {
+        csr_write(index, prev_value | mem_register_read(rs1));
+    }
+}
+
+void csr_csrrc(uint8_t rs1, uint8_t rd, uint16_t csr_number) {
+    uint8_t index = csr_get_index_by_number(csr_number);
+    if (index == (uint8_t) -1) {
+        /* Raise exception */
+        return;
+    }
+    uint32_t prev_value = csr_read(index);
+    mem_register_write(rd, prev_value);
+    if (rs1) {
+        csr_write(index, prev_value & (~mem_register_read(rs1)));
+    }
 }
