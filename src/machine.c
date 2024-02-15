910c2434322abeb548a70a04847f8654b89bd1c5
gtxzsxxk <2524395907@qq.com>
2024-02-16 00:20:42 +0800

    Revert "machine: add debug test marco"
    This reverts commit 47af81ba02a2ca8a86a004dc6b4ce72d3c47da66.

--- src/machine.c
+++ src/machine.c
@@ -4,23 +4,18 @@
 #include "mem.h"
 #include "decode.h"
 
-#define RISCV_DEBUG
 #define RISCV_ISA_TESTS
 
 void machine_start(void) {
     program_counter = ROM_START_ADDR;
     for (;;) {
         uint32_t instruction = mem_read_w(program_counter);
-
-#ifdef RISCV_DEBUG
         mem_debug_printreg(program_counter);
 #ifdef RISCV_ISA_TESTS
         if ((instruction & 0x7f) == RV32I_ZICSR_ECALL_EBREAK) {
             return;
         }
 #endif
-#endif
-
         decode(instruction);
     }
 }
