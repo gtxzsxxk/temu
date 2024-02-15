58f2c8e2aaccdf1339e29e3ca81eac9e6d55b931
gtxzsxxk <2524395907@qq.com>
2024-02-16 00:20:42 +0800

    Revert "test: add ieee754 float and double convert and calc test"
    This reverts commit 2c466f8d1068a62c10ef815f3ba6859acac779ee.

--- test/bare-metal/ieee754.c
+++ test/bare-metal/ieee754.c
@@ -1,13 +1,7 @@
-int test() {
+int main() {
     float a = 19.6;
     float d = 7.8;
     int c = 114;
     double e = 3.14;
     return (int) (a * d + c / e);
-}
-
-int main() {
-    test();
-    __asm__("ecall");
-    return 0;
 }
