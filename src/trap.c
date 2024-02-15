291d67f93990c9f474f81fc6ad67dedbfeee95ae
gtxzsxxk <2524395907@qq.com>
2024-02-16 00:20:42 +0800

    Revert "trap: exception should always be allowed"
    This reverts commit 2aa8fa2e2271996014c869b4f2e3b8e800ce69bb.

--- src/trap.c
+++ src/trap.c
@@ -12,22 +12,21 @@ void trap_throw_interrupt(uint32_t cause) {
 }
 
 void trap_throw_exception(uint32_t cause) {
-    /* set cause */
-    control_status_registers[CSR_idx_mcause] = cause;
-    control_status_registers[CSR_idx_mcause] &= 0x7fffffff;
-    /* set previous interrupt enable */
-    control_status_registers[CSR_idx_mstatus] |= (1 << mstatus_MPIE);
-    /* disable interrupt */
-    control_status_registers[CSR_idx_mstatus] &= ~(1 << mstatus_MIE);
-    control_status_registers[CSR_idx_mepc] = program_counter;
-    program_counter = control_status_registers[CSR_idx_mtvec];
-    if (control_status_registers[CSR_idx_mtvec] & 0x01) {
-        /* Vectored */
-        program_counter += 4 * cause;
+    if (control_status_registers[CSR_idx_mstatus] & (1 << mstatus_MIE)) {
+        /* set previous interrupt enable */
+        control_status_registers[CSR_idx_mstatus] |= (1 << mstatus_MPIE);
+        /* disable interrupt */
+        control_status_registers[CSR_idx_mstatus] &= ~(1 << mstatus_MIE);
+        control_status_registers[CSR_idx_mepc] = program_counter;
+        program_counter = control_status_registers[CSR_idx_mtvec];
+        if (control_status_registers[CSR_idx_mtvec] & 0x01) {
+            /* Vectored */
+            program_counter += 4 * cause;
+        }
+        control_status_registers[CSR_idx_mstatus] &= ~(3 << mstatus_MPP);
+        control_status_registers[CSR_idx_mstatus] |= (current_privilege << mstatus_MPP);
+        current_privilege = CSR_MASK_MACHINE;
     }
-    control_status_registers[CSR_idx_mstatus] &= ~(3 << mstatus_MPP);
-    control_status_registers[CSR_idx_mstatus] |= (current_privilege << mstatus_MPP);
-    current_privilege = CSR_MASK_MACHINE;
 }
 
 void trap_return_machine(void) {
