d3119c3d54ea8f5911baf3ed4d26e85974b1a28e
gtxzsxxk <2524395907@qq.com>
2024-02-16 00:20:42 +0800

    Revert "machine: add marco for riscv isa tests"
    This reverts commit 8116a2bb80a20bd271a5adece6ebce9d81cf580a.

--- src/machine.c
+++ src/machine.c
@@ -4,18 +4,11 @@
 #include "mem.h"
 #include "decode.h"
 
-#define RISCV_ISA_TESTS
-
 void machine_start(void) {
     program_counter = ROM_START_ADDR;
-    for (;;) {
+    for(;;) {
         uint32_t instruction = mem_read_w(program_counter);
         decode(instruction);
         mem_debug_printreg();
-#ifdef RISCV_ISA_TESTS
-        if ((instruction & 0x7f) == RV32I_ZICSR_ECALL_EBREAK) {
-            int a = 0;
-        }
-#endif
     }
 }
