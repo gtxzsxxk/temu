d1457ca2fec004217ad47d5e5f65540713441a5f
gtxzsxxk <2524395907@qq.com>
2024-02-16 00:20:42 +0800

    Revert "mem: add memory map for uart peripheral"
    This reverts commit 9a9dbd306d26ada05293ec06d3989286377620e8.

--- include/parameters.h
+++ include/parameters.h
@@ -5,12 +5,9 @@
 #ifndef TEMU_PARAMETERS_H
 #define TEMU_PARAMETERS_H
 
-#define ROM_BASE_ADDR           0x00000000
-#define ROM_SIZE                0x01000000
-#define RAM_BASE_ADDR           0x02000000
-#define RAM_SIZE                0x01000000
-
-#define UART_BASE_ADDR          0x12500000
-#define UART_SIZE               0x00000100
+#define ROM_BASE_ADDR 0x00000000
+#define ROM_SIZE       0x01000000
+#define RAM_BASE_ADDR 0x02000000
+#define RAM_SIZE       0x01000000
 
 #endif //TEMU_PARAMETERS_H
