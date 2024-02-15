eb492b34e80c28b6738ed37b51b2172e08551205
gtxzsxxk <2524395907@qq.com>
2024-02-16 00:20:42 +0800

    Revert "util: csrIndexGen.py be able to generate the index and set up the csr_index_remap"
    This reverts commit 2369c6f40fc5e98c793935ca3ee99a67a98d68e8.

--- src/zicsr.c
+++ src/zicsr.c
@@ -6,10 +6,7 @@
 
 uint32_t control_status_registers[CSR_SIZE];
 uint8_t csr_privilege[CSR_SIZE];
-uint8_t csr_index_remap[0x10] = {
-        [0x1] = CSR_idx_sstatus,
-        [0x3] = CSR_idx_mstatus,
-        [0x5] = CSR_idx_scontext,
-        [0xC] = CSR_idx_cycle,
-        [0xF] = CSR_idx_mvendorid,
+uint8_t csr_index_remap[0xf] = {
+        [0xC] = 0,
+        []
 };
--- util/csrIndexGen.py
+++ util/csrIndexGen.py
@@ -14,13 +14,6 @@ class CsrInformation:
     def __str__(self):
         return "CSR NAME=%s\n    NUMBER=%s\n    PRIV=%s\n\n" % (self.csrName, self.csrNumber, self.csrPrivilege)
 
-    def to_c_definitions(self, index):
-        return """#define CSR_num_%s %s
-#define CSR_prv_%s %s
-#define CSR_idx_%s %d
-
-""" % (self.csrName, self.csrNumber, self.csrName, self.csrPrivilege, self.csrName, index)
-
 
 zicsrSource = '../include/zicsr.h'
 matchBegin = '/* === csrIndexGen match begin DO NOT MODIFY THIS LINE === */'
@@ -31,27 +24,24 @@ codeLines = fp.readlines()
 fp.close()
 
 startIndex = 0
-startPos = 0
 endIndex = 0
 csrList = []
 
 
 def read_csr_definitions():
-    global csrList, startIndex, endIndex
+    global csrList, startIndex
     while not codeLines[startIndex + 1].__contains__(matchEnd):
         def_index = startIndex + 1
         def_end_index = def_index + 1
         def_lines = [codeLines[def_index]]
         while codeLines[def_end_index] != '\r\n' and codeLines[def_end_index] != '\n':
             if codeLines[def_end_index].__contains__(matchEnd):
-                endIndex = def_end_index
                 csrList.append(CsrInformation(def_lines))
                 return
             def_lines.append(codeLines[def_end_index])
             def_end_index += 1
         startIndex = def_end_index
         csrList.append(CsrInformation(def_lines))
-    endIndex = startIndex + 1
 
 
 for i in range(0, codeLines.__len__()):
@@ -59,19 +49,7 @@ for i in range(0, codeLines.__len__()):
         continue
     if codeLines[i].__contains__(matchBegin):
         startIndex = i
-        startPos = i
         read_csr_definitions()
         break
 
-csrList.sort(key=lambda elem: eval(elem.csrNumber))
-fp = open(zicsrSource, "w")
-for i in range(0, startPos + 1):
-    fp.write(codeLines[i])
-
-for i in range(0, csrList.__len__()):
-    fp.write(csrList[i].to_c_definitions(i))
-
-for i in range(endIndex, codeLines.__len__()):
-    fp.write(codeLines[i])
-
-fp.close()
+print(csrList)
