090c10e0778bab5c03974fb2962784cdbb58bb82
gtxzsxxk <2524395907@qq.com>
2024-02-18 00:20:10 +0800

    machine: refactor the code

--- src/machine.c
+++ src/machine.c
@@ -13,20 +13,18 @@
 #define RISCV_DEBUG
 #define RISCV_ISA_TESTS
 
-static void set_terminal(void) {
-    static struct termios tm;
-    tcgetattr( STDIN_FILENO, &tm);
-    cfmakeraw(&tm);
-    tm.c_lflag &= ~(ICANON);
-    tcsetattr( STDIN_FILENO, TCSANOW, &tm);
-}
+static uint8_t access_error_intr;
+
+static void machine_pre_boot(uint32_t start);
+
+static void machine_tick(void);
+
+static void machine_debug(uint32_t instruction, int printreg);
+
+static void set_terminal(void);
 
 _Noreturn void machine_start(uint32_t start, int printreg) {
-    uint8_t access_error_intr;
-    program_counter = start;
-
-    set_terminal();
-    uart8250_init();
+    machine_pre_boot(start);
 
     for (;;) {
         access_error_intr = 0;
@@ -34,28 +32,50 @@ _Noreturn void machine_start(uint32_t start, int printreg) {
         if (access_error_intr) {
             trap_throw_exception(EXCEPTION_INST_ACCESS_FAULT);
         } else {
-#ifdef RISCV_DEBUG
-            if (printreg) {
-                mem_debug_printreg(program_counter);
-            }
-#ifdef RISCV_ISA_TESTS
-            if (program_counter == 0x104) {
-                int a = 0;
-            }
-            if (instruction == 0x00000073) {
-                int a = 0;
-            }
-#endif
-#endif
-
             decode(instruction);
         }
-        uart8250_tick();
-        zicnt_cycle_tick();
-
-        if(zicnt_get_cycle() % SIM_YIELD_GAP == 0){
-            zicnt_time_tick();
-            usleep(SIM_YIELD_TIME);
-        }
+        machine_debug(instruction, printreg);
+        machine_tick();
     }
+}
+
+static void machine_pre_boot(uint32_t start) {
+    program_counter = start;
+
+    set_terminal();
+    uart8250_init();
+}
+
+static void machine_tick(void) {
+    uart8250_tick();
+    zicnt_cycle_tick();
+
+    if (zicnt_get_cycle() % SIM_YIELD_GAP == 0) {
+        zicnt_time_tick();
+        usleep(SIM_YIELD_TIME);
+    }
+}
+
+static void machine_debug(uint32_t instruction, int printreg) {
+#ifdef RISCV_DEBUG
+    if (printreg) {
+        mem_debug_printreg(program_counter);
+    }
+#ifdef RISCV_ISA_TESTS
+    if (program_counter == 0xbfdffc4) {
+        int a = 0;
+    }
+    if (instruction == 0x00000073) {
+        int a = 0;
+    }
+#endif
+#endif
+}
+
+static void set_terminal(void) {
+    static struct termios tm;
+    tcgetattr(STDIN_FILENO, &tm);
+    cfmakeraw(&tm);
+    tm.c_lflag &= ~(ICANON);
+    tcsetattr(STDIN_FILENO, TCSANOW, &tm);
 }
