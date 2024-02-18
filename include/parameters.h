4f870a34d203edaa6525c1abd81b9e6306336681
gtxzsxxk <2524395907@qq.com>
2024-02-19 00:46:47 +0800

    top: final decision: set ROM size to 64KiB and RAM to 16MiB

--- include/parameters.h
+++ include/parameters.h
@@ -6,9 +6,9 @@
 #define TEMU_PARAMETERS_H
 
 #define ROM_BASE_ADDR           0x00000000
-#define ROM_SIZE                0x01000000
+#define ROM_SIZE                0x00010000
 #define RAM_BASE_ADDR           0x02000000
-#define RAM_SIZE                0x0a000000
+#define RAM_SIZE                0x01000000
 
 #define UART_BASE_ADDR          0x12500000
 #define UART_SIZE               0x00000100
