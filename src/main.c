8b2b653b8f9cf122691ba82f838e2df30492212a
gtxzsxxk <2524395907@qq.com>
2024-02-19 13:43:42 +0800

    top: add usage description

--- src/main.c
+++ src/main.c
@@ -7,7 +7,13 @@
 #include "machine.h"
 #include "mem.h"
 
-static const char *usage = "Usage: temu [-ram/-rom/-addr 0x02000000] [-printreg] -exec=program.bin -with=0x03000000#u-boot.bin\r\n";
+static const char *usage = "Usage: temu [-ram/-rom/-addr 0x02000000] [-printreg] -exec=program.bin [-with=addr#file.bin]\n\n"
+                           "Example:\n"
+                           "--ram\n"
+                           "--exec=fw_jump.bin\n"
+                           "--with=0x0204b000#u-boot.bin\n"
+                           "--with=0x02048800#u-boot.dtb\n"
+                           "--with=0x028cb000#Image.gz\n";
 
 static int load_binary(uint32_t addr, const char *path);
 
