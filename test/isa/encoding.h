cdb589a9846cbda6b5906953a58e26d8c331ae77
gtxzsxxk <2524395907@qq.com>
2024-02-21 15:07:12 +0800

    test: set DRAM_BASE to 0x0200_0000

--- test/isa/encoding.h
+++ test/isa/encoding.h
@@ -300,7 +300,7 @@
 #define CLINT_BASE         0x02000000
 #define CLINT_SIZE         0x000c0000
 #define EXT_IO_BASE        0x40000000
-#define DRAM_BASE          0x80000000
+#define DRAM_BASE          0x02000000
 
 /* page table entry (PTE) fields */
 #define PTE_V     0x001 /* Valid */
