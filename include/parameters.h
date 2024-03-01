25d96f15d8a0e025d80964ad100f3bb796681e48
gtxzsxxk <2524395907@qq.com>
2024-03-01 12:23:07 +0800

    parameter: set the RAM base address to 0x80000000

--- include/parameters.h
+++ include/parameters.h
@@ -7,7 +7,7 @@
 
 #define ROM_BASE_ADDR           0x00000000
 #define ROM_SIZE                0x00010000
-#define RAM_BASE_ADDR           0x02000000
+#define RAM_BASE_ADDR           0x80000000
 #define RAM_SIZE                0x01000000
 
 #define UART_BASE_ADDR          0x12500000
@@ -16,7 +16,7 @@
 /* 均摊时钟 in MHz */
 #define SIM_EVENLY_DIV_CLOCK    100
 /* 缩放比例 */
-#define SIM_CLOCK_SCALE    10
+#define SIM_CLOCK_SCALE         1
 /* 执行多少条指令后，统一给出CPU */
 #define SIM_YIELD_GAP           10000
 /* 给出CPU的时间(us) */
