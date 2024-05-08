9ededdf26a51c020c960858cfab8887fb69d3689
Hanyuan Zhao <hanyuan-z@qq.com>
2024-05-08 21:02:17 +0800

    TLB: refactor the cache line tag and index design to better fit the locality of reference

--- include/tlb.h
+++ include/tlb.h
@@ -7,21 +7,22 @@
 
 #include <stdint.h>
 
-#define TLB_CACHE_LINE_SIZE_LOG     10
-#define TLB_CACHE_LINE_SIZE         (1 << TLB_CACHE_LINE_SIZE_LOG)
-#define SV32_VPN_FIELD_LENGTH       20
-#define TLB_CACHE_WAY               1   /* 由于大小限制，退化为直接映射 */
+#define TLB_CACHE_LINE_FIELD_LENGTH     10
+#define TLB_CACHE_LINE_SIZE             (1 << TLB_CACHE_LINE_FIELD_LENGTH)
+#define SV32_PAGE_OFFSET_FIELD_LEN      12
+#define SV32_VPN_FIELD_LENGTH           (32 - SV32_PAGE_OFFSET_FIELD_LEN)
+#define TLB_CACHE_TAG_FIELD_LENGTH      (SV32_VPN_FIELD_LENGTH - TLB_CACHE_LINE_FIELD_LENGTH)
+#define TLB_CACHE_WAY                   1   /* 由于大小限制，退化为直接映射 */
 
-#define TLB_MISS_IDENTIFIER         1
-#define TLB_PAGE_FAULT_IDENTIFIER   2
+#define TLB_MISS_IDENTIFIER             1
+#define TLB_PAGE_FAULT_IDENTIFIER       2
 
-#define TLB_VADDR_GET_INDEX(x)      (x >> (32 - TLB_CACHE_LINE_SIZE_LOG))
-#define TLB_VADDR_GET_TAG(x)        (x >> (32 - TLB_CACHE_LINE_SIZE_LOG - (SV32_VPN_FIELD_LENGTH - TLB_CACHE_LINE_SIZE_LOG)) \
-                                    & (0xffffffff >> (32 - (SV32_VPN_FIELD_LENGTH - TLB_CACHE_LINE_SIZE_LOG))))
+#define TLB_VADDR_GET_INDEX(x)      ((x >> SV32_PAGE_OFFSET_FIELD_LEN) & (0xffffffff >> (32 - TLB_CACHE_LINE_FIELD_LENGTH)))
+#define TLB_VADDR_GET_TAG(x)        (x >> (SV32_PAGE_OFFSET_FIELD_LEN + TLB_CACHE_LINE_FIELD_LENGTH))
 
 /* 组相联 */
 struct tlb_cache_line {
-    uint16_t tag: (SV32_VPN_FIELD_LENGTH - TLB_CACHE_LINE_SIZE_LOG);
+    uint16_t tag: TLB_CACHE_TAG_FIELD_LENGTH;
     uint16_t access_counter;
     uint32_t ppn: SV32_VPN_FIELD_LENGTH;
     uint8_t valid: 1;
