169d58b85f967da560003736712faf8dc49a8af5
gtxzsxxk <2524395907@qq.com>
2024-05-08 18:23:50 +0800

    main_memory: set the virtual memory bus to 32-bit wide to speed up I/O

--- src/vm.c
+++ src/vm.c
@@ -1,12 +1,16 @@
 //
 // Created by hanyuan on 2024/2/20.
 //
-#include <stdio.h>
 #include <stdint.h>
 #include <parameters.h>
 #include "vm.h"
+#include "cache.h"
 #include "zicsr.h"
 #include "port/main_memory.h"
+
+#ifndef NULL
+#define NULL (void*)0
+#endif
 
 uint8_t vm_on(void) {
     return current_privilege <= CSR_MASK_SUPERVISOR && control_status_registers[CSR_idx_satp];
@@ -29,9 +33,9 @@ uint8_t vm_status_read_sum() {
 static uint32_t vm_translation(uint32_t vaddr, uint8_t *page_fault, uint8_t *access_flags, uint8_t *user_only) {
     uint32_t pgtable = SV32_ROOT(control_status_registers[CSR_idx_satp]);
     uint32_t pte;
-    uint8_t intr;
+    uint8_t intr = 0;
     for (int i = 1; i >= 0; i--) {
-        pte = pm_read_w(pgtable + SV32_VPN(vaddr, i) * 4, &intr);
+        pte = cache_data_read_w(pgtable + SV32_VPN(vaddr, i) * 4, &intr);
         if (!PTE_MATCH(pte, PTE_V) || (!PTE_MATCH(pte, PTE_R) && PTE_MATCH(pte, PTE_W))) {
             if (page_fault) {
                 *page_fault = 1;
@@ -60,7 +64,7 @@ static uint32_t vm_translation(uint32_t vaddr, uint8_t *page_fault, uint8_t *acc
             *user_only = PTE_MATCH(pte, PTE_U);
             *access_flags = (pte >> 1) & 0x07;
             /* the INTR shouldn't be 1 */
-            pm_write_w(pgtable + SV32_VPN(vaddr, i) * 4, pte, NULL);
+            cache_data_write_w(pgtable + SV32_VPN(vaddr, i) * 4, pte, NULL);
             uint32_t pa = SV32_PTE2PA(pte) + SV32_VOFFSET(vaddr);
             if (i == 1) {
                 /* superpage */
@@ -103,96 +107,15 @@ uint32_t vm_lookup_paddr(uint32_t vaddr, uint8_t *page_fault, uint8_t access_fla
     return 0x9a9a9a9a;
 }
 
+/* Used for debug only */
 uint8_t pm_read_b(uint32_t addr, uint8_t *intr) {
     if (addr >= RAM_BASE_ADDR && addr < RAM_BASE_ADDR + RAM_SIZE) {
-        return port_main_memory_read(addr - RAM_BASE_ADDR);
+        uint8_t offset = addr & 0x03;
+        return port_main_memory_read_w((addr & 0xfffffffc) - RAM_BASE_ADDR) >> offset;
     } else {
         if (intr) {
             *intr = 1;
         }
         return 0xff;
-    }
-}
-
-uint16_t pm_read_h(uint32_t addr, uint8_t *intr) {
-    if (addr % 2) {
-        if (intr) {
-            *intr = 3;
-        }
-        return 0xff;
-    }
-    if (addr >= RAM_BASE_ADDR && addr + 1 < RAM_BASE_ADDR + RAM_SIZE) {
-        return port_main_memory_read(addr - RAM_BASE_ADDR) | (port_main_memory_read(addr - RAM_BASE_ADDR + 1) << 8);
-    } else {
-        if (intr) {
-            *intr = 1;
-        }
-        return 0xffff;
-    }
-}
-
-uint32_t pm_read_w(uint32_t addr, uint8_t *intr) {
-    if (addr % 4) {
-        if (intr) {
-            *intr = 3;
-        }
-        return 0xff;
-    }
-    if (addr >= RAM_BASE_ADDR && addr + 3 < RAM_BASE_ADDR + RAM_SIZE) {
-        return
-                port_main_memory_read(addr - RAM_BASE_ADDR) |
-                (port_main_memory_read(addr - RAM_BASE_ADDR + 1) << 8) |
-                (port_main_memory_read(addr - RAM_BASE_ADDR + 2) << 16) |
-                (port_main_memory_read(addr - RAM_BASE_ADDR + 3) << 24);
-    } else {
-        if (intr) {
-            *intr = 1;
-        }
-
-        return 0x6666ffff;
-    }
-}
-
-void pm_write_b(uint32_t addr, uint8_t data, uint8_t *intr) {
-    if (addr >= RAM_BASE_ADDR && addr < RAM_BASE_ADDR + RAM_SIZE) {
-        port_main_memory_write(addr - RAM_BASE_ADDR, data);
-    } else {
-        if (intr) {
-            *intr = 1;
-        }
-    }
-}
-
-void pm_write_h(uint32_t addr, uint16_t data, uint8_t *intr) {
-    if (addr % 2) {
-        if (intr) {
-            *intr = 3;
-        }
-    }
-    if (addr >= RAM_BASE_ADDR && addr + 1 < RAM_BASE_ADDR + RAM_SIZE) {
-        port_main_memory_write(addr - RAM_BASE_ADDR, data & 0xff);
-        port_main_memory_write(addr - RAM_BASE_ADDR + 1, (data >> 8) & 0xff);
-    } else {
-        if (intr) {
-            *intr = 1;
-        }
-    }
-}
-
-void pm_write_w(uint32_t addr, uint32_t data, uint8_t *intr) {
-    if (addr % 4) {
-        if (intr) {
-            *intr = 3;
-        }
-    }
-    if (addr >= RAM_BASE_ADDR && addr + 3 < RAM_BASE_ADDR + RAM_SIZE) {
-        port_main_memory_write(addr - RAM_BASE_ADDR, data & 0xff);
-        port_main_memory_write(addr - RAM_BASE_ADDR + 1, (data >> 8) & 0xff);
-        port_main_memory_write(addr - RAM_BASE_ADDR + 2, (data >> 16) & 0xff);
-        port_main_memory_write(addr - RAM_BASE_ADDR + 3, (data >> 24) & 0xff);
-    } else {
-        if (intr) {
-            *intr = 1;
-        }
     }
 }
