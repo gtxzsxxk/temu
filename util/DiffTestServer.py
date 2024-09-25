66f451f1a7ab4c0cd38b271774d45be0b0757df7
Hanyuan Zhao <hanyuan-z@qq.com>
2024-09-25 14:21:43 +0800

    difftest: add support for continuously execute a batch of instructions

--- util/DiffTestServer.py
+++ util/DiffTestServer.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import ctypes
+import json
 
 DIFFTEST_PATH = "../cmake-build-debug/libtemudifftest.dylib"
 temuHandler = None
@@ -37,6 +38,54 @@ def temuExecInstruction(instStr):
             result["registers"].append("0x%08x" % (temuHandler.mmu_register_read(i) & 0xffffffff))
 
         return jsonify(result)
+    else:
+        return jsonify({
+            "valid": False,
+            "message": "You need to reset temu."
+        })
+
+
+@app.route("/exec/batch/<compareWhat>", methods=['POST'])
+def temuExecInstBatch(compareWhat):
+    data = request.get_data()
+    data = json.loads(data)
+    if temuHandler:
+        if compareWhat == "pcAndFinalReg":
+            result = {
+                "valid": True,
+                "registers": [],
+                "pc_history": []
+            }
+            for instStr in data["instructions"]:
+                inst = ctypes.c_uint32(eval("0x" + instStr))
+                temuHandler.decode(inst)
+                result["pc_history"].append("0x%08x" % (temuHandler.lib_get_program_counter() & 0xffffffff))
+
+            for i in range(0, 32):
+                result["registers"].append("0x%08x" % (temuHandler.mmu_register_read(i) & 0xffffffff))
+            return jsonify(result)
+        elif compareWhat == "pcAndEachReg":
+            result = {
+                "valid": True,
+                "registers": [],
+                "pc_history": []
+            }
+            for instStr in data["instructions"]:
+                inst = ctypes.c_uint32(eval("0x" + instStr))
+                temuHandler.decode(inst)
+                result["pc_history"].append("0x%08x" % (temuHandler.lib_get_program_counter() & 0xffffffff))
+                reg_hist = []
+                for i in range(0, 32):
+                    reg_hist.append("0x%08x" % (temuHandler.mmu_register_read(i) & 0xffffffff))
+
+                result["registers"].append(reg_hist)
+
+            return jsonify(result)
+        else:
+            return jsonify({
+                "valid": False,
+                "message": "Not supported compare method settings."
+            })
     else:
         return jsonify({
             "valid": False,
